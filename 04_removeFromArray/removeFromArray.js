a9048dc602cc8eaf6105bafd58883dc78db63d81
joelbaldapan <baldapan.joelangelo@gmail.com>
2024-06-02 19:33:29 +0800

    Finish 03

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+    let array = arguments[0];
 
+    for (let i = 1; i < arguments.length; i++) {
+        const arg = arguments[i]
+        // Find how many instances to be removed
+        const total = (array.filter(item => item === arg)).length;
+
+        // Remove each of them
+        for (let j = 0; j < total; j++) {
+            let index = array.findIndex(item => item === arg);
+            array.splice(index, 1);
+        }
+    }
+
+    return array;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
