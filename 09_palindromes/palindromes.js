ac6f8169546b83ea720bc5b85b7a7b5137b0bcf4
joelbaldapan <baldapan.joelangelo@gmail.com>
2024-06-17 00:01:00 +0800

    Finish 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const alphaNumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  const cleanString = string
+    .toLowerCase()
+    .split("")
+    .filter((letter) => alphaNumerical.includes(letter))
+    .join("");
+
+  const reversed = cleanString.split("").reverse().join("");
+  return cleanString === reversed;
 };
 
 // Do not edit below this line
