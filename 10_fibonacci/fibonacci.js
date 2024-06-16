612a024d320e3f7bbe1b44745a8641f1ad6e2cad
joelbaldapan <baldapan.joelangelo@gmail.com>
2024-06-17 00:15:07 +0800

    Finish 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (position) {
+  if (position < 0) {
+    return "OOPS";
+  }
+  const pos = +position;
+  let prev = 1;
+  let current = 0;
+  for (i = 0; i < pos; i++) {
+    current += prev;
+    prev = current - prev;
+  }
+  console.log(current);
+  return current;
 };
 
 // Do not edit below this line
