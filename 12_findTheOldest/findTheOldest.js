4fed249321d878b07cd972010269bbeed895e46a
joelbaldapan <baldapan.joelangelo@gmail.com>
2024-06-17 04:20:33 +0800

    Finish 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const date = new Date();
+  let year = date.getFullYear();
 
+  people.forEach((person) => {
+    if (person.yearOfDeath === undefined) person.yearOfDeath = year;
+  });
+
+  const mappedAge = people.map(
+    (person) => person.yearOfDeath - person.yearOfBirth
+  );
+  const biggestAge = mappedAge.reduce((biggest, current) =>
+    current > biggest ? current : biggest
+  );
+
+  return people[mappedAge.indexOf(biggestAge)];
 };
 
 // Do not edit below this line
